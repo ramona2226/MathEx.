f54179f73043e048952b6b4f84d2d6c3440121be
Ramona2226 <brisc.ramona22@yahoo.com>
2020-06-16 06:25:58 +0300

    math operation added

--- src/main/java/org/fastTrackIT/App.java
+++ src/main/java/org/fastTrackIT/App.java
@@ -1,36 +1,49 @@
 package org.fastTrackIT;
 
 
-import java.util.Scanner;
-
-public class App
-{
-    public static void main( String[] args ) {
+public class App {
+    // public static void main( String[] args ) {
 
 
-        System.out.println("Enter the first number");
-        Scanner scanner = new Scanner(System.in);
-        int firstNumber = scanner.nextInt();
-
-        System.out.println("Enter the second number");
-        Scanner scanner1 = new Scanner(System.in);
-        int secondNumber = scanner1.nextInt();
-
-        Classroom classroom = new Classroom () ;
-
-        classroom.sum(firstNumber, secondNumber);
-
-//        classroom.name = "ButterflysClass";
-//        classroom.age = 3;
+//        System.out.println("Enter the first number");
+//        Scanner scanner = new Scanner(System.in);
+//        int firstNumber = scanner.nextInt();
 //
+//        System.out.println("Enter the second number");
+//        Scanner scanner1 = new Scanner(System.in);
+//        int secondNumber = scanner1.nextInt();
 //
-//        System.out.println(classroom.name);
-//        System.out.println(classroom.age);
+//        Classroom classroom = new Classroom () ;
+//
+//        classroom.sum(firstNumber, secondNumber);
 
-//ti-am facut un model
-        // ma inclin inaintea ta .
-        //nu trebuie, flaviu e bossul:)
-        //tu esti ajutorul meu
-        //spor:)
-   }
+    public static void main(String[] args) {
+
+
+        // initializing variables
+        int number1 = 10;
+        int number2 = 5;
+
+
+        System.out.println(number1);
+        System.out.println(number2);
+
+       int sum = number1 + number2;
+        System.out.println("Sum is:" + sum);
+
+        int sub = number1 - number2;
+        System.out.println("Subtraction is:" + sub);
+
+        int mult = number1 * number2;
+        System.out.println("Multiplication is:" + mult);
+
+        int div = number1 / number2;
+        System.out.println("Divizion is:" + div);
+
+
+
+
+    }
+
 }
+
